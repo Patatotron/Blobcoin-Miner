9ad50907d687eb26095f8e11b24d2902a0c7d9aa
Thomas <124117373+Patatotron@users.noreply.github.com>
2023-05-07 16:26:36 -0400

    Add files via upload

--- crypto_miner.py
+++ crypto_miner.py
@@ -1,14 +1,16 @@
 import random
 import string
 import hashlib
+from concurrent.futures import ProcessPoolExecutor
 question = str(input("Do you want to have the easy copy paste option on? (y/n): "))
 repeat = int(input("how many blobcoin do you want to find?: "))
 letter = str(string.ascii_lowercase)
 dif = int(input("Enter difficulty level: "))
+digit = int(input("Enter number of digits does the tested numbers should have: "))
 check = ''.join('0' for i in range(dif))
 x = 0
 while x < repeat:
-    a = ''.join(random.choice(letter) for b in range(32))
+    a = ''.join(random.choice(letter) for b in range(digit))
     c = hashlib.sha256(a.encode("utf")).hexdigest()
     if c.startswith(check):
         if x != -1:
